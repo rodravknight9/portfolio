fb8eea3c548ef829286ed504bf7cc3a3dccebdde
rodravknight <dealarcmarcel99@gmail.com>
2025-08-02 13:02:03 -0400

    Plane is flying

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Plane from './components/Plane';
+
+function App() {
+
+   return (
+    <>
+      <Plane />
+    </>
+  );
+}
+
+export default App
---
+++ src/components/Plane.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Canvas, useThree } from '@react-three/fiber';
+import { useGLTF, OrbitControls, useAnimations } from '@react-three/drei';
+import { useEffect, useRef } from 'react';
+import * as THREE from 'three';
+
+function Model() {
+  const glft = useGLTF('/planeColors.glb');
+  const modelRef = useRef();
+  const { camera } = useThree();
+  const { actions } = useAnimations(glft.animations, modelRef);
+
+  useEffect(() => {
+   const model = modelRef.current;
+    if (!model) return;
+
+    const box = new THREE.Box3().setFromObject(modelRef.current);
+    const center = box.getCenter(new THREE.Vector3());
+    modelRef.current.position.sub(center);
+
+    const size = box.getSize(new THREE.Vector3()).length();
+    const scale = 3;
+    model.scale.set(scale, scale, scale);
+
+    const distance = (size * 1.8) / scale;
+    camera.position.set(0, 0, distance);
+    camera.lookAt(0, 0, 0);
+
+    actions['Flying'].play();
+
+  }, [glft, camera, actions]);
+  
+  return (
+    <mesh ref={modelRef} rotation={[0, -0.8, 0]}>
+        <primitive object={glft.scene} />
+    </mesh>
+  );
+}
+
+
+const Plane = () => {
+    return (
+        <div className='w-full h-screen z-10 h-screen w-screen bg-sky'>
+            <Canvas>
+                <directionalLight position={[10, 4, 5]} intensity={8} />
+                <ambientLight intensity={6} />
+                <pointLight position={[10, 5, 10]} intensity={2} />
+                <spotLight position={[20, 50, 10]} angle={0.15} penumbra={1} intensity={1} />
+                <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
+                <Model  />
+            </Canvas>
+        </div>
+    )
+}
+
+export default Plane
